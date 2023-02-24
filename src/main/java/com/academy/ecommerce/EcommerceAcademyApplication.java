7af66de031be2df31d7ebd6d805de5a7bba78735
Andrei Cinca <andrei.cinca@agm.local>
2023-02-24 14:24:25 +0100

    inital version

---
+++ src/main/java/com/academy/ecommerce/EcommerceAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.academy.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/ecommerce/EcommerceAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
